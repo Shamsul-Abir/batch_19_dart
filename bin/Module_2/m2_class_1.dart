585bf2e92b5a08c57d880bf03cafd2141fdec732
Shamsul-Abir <samsulabir007@gmail.com>
2026-09-10 11:39:16 +0600

    changed

--- bin/Module_2/m2_class_1.dart
+++ bin/Module_2/m2_class_1.dart
@@ -1,4 +1,4 @@
-//if else, switch case and Loop
+//if else, switch case and Loop --> For, DO, Do-While
 
 main(){
 
@@ -30,8 +30,27 @@ main(){
     default:
       print('Bashai');
   }
+  
+  print('\n');
 
-  for(int i=0; i<=5; i++){
+  for(int i=1; i<=10; i++){
     print('12 x $i = ${12*1}');
   }
+
+  print('\n');
+
+  int j = 1;
+  while(j <= 10){
+    print('13 x $j = ${13*j}');
+    j++;
+  }
+
+  print('\n');
+
+  //do-while will do the work first than it will check the condition
+  int k =1;
+  do{
+    print('15 x $k = ${13*k}');
+    k++;
+  }while (k<=10);
 }
